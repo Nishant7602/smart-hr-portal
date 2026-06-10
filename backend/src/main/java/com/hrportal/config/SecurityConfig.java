fd592c4af1677bc8abf78cdcbaaa85be057bdaef
Nishant Khandelwal <nishantkhandelwal210400@gmail.com>
2026-06-10 19:07:58 +0530

    fix: update something

--- backend/src/main/java/com/hrportal/config/SecurityConfig.java
+++ backend/src/main/java/com/hrportal/config/SecurityConfig.java
@@ -57,14 +57,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:3001"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://localhost:3001","https://smart-hr-portal-nhtt.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowedOrigins(List.of(
-        	    "http://localhost:3000",
-        	    "http://localhost:5173",
-        	    "https://smart-hr-portal-nhtt.onrender.com"
-        	));
         config.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
